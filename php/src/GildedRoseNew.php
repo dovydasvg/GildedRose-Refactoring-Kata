aeed40c0cbd4c6cce312da8c864a4a7c6a6cc10f
dovydasvg <dovydasvg@yahoo.fr>
2021-04-18 13:41:22 +0300

    Fixed item reading for quality > 50 and < 0

--- php/src/GildedRoseNew.php
+++ php/src/GildedRoseNew.php
@@ -99,9 +99,9 @@ final class GildedRoseNew
             $item->quality += $ChangeQuality;
 
             // Quality must be between 0 and 50
-            if($quality > 50){
+            if($item->quality > 50){
                 $item->quality = 50;
-            }elseif($quality < 0){
+            }elseif($item->quality < 0){
                 $item->quality = 0;
             }
 
