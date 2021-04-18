ce9439d14b48cb22c411ef92992e911ebf81c38e
dovydasvg <dovydasvg@yahoo.fr>
2021-04-18 14:01:17 +0300

    Fixed conjured Approval test

--- php/fixtures/texttest_fixture.php
+++ php/fixtures/texttest_fixture.php
@@ -4,6 +4,7 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 use GildedRose\GildedRose;
 use GildedRose\Item;
+use GildedRose\GildedRoseNew;
 
 echo "OMGHAI!" . PHP_EOL;
 
@@ -16,11 +17,10 @@ $items = array(
     new Item('Backstage passes to a TAFKAL80ETC concert', 15, 20),
     new Item('Backstage passes to a TAFKAL80ETC concert', 10, 49),
     new Item('Backstage passes to a TAFKAL80ETC concert', 5, 49),
-    // this conjured item does not work properly yet
     new Item('Conjured Mana Cake', 3, 6)
 );
 
-$app = new GildedRose($items);
+$app = new GildedRoseNew($items);
 
 $days = 2;
 if (count($argv) > 1) {
