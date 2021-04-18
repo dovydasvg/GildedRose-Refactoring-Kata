7e98884c14edcf453b14eb3df94c29cfeafe8dfa
dovydasvg <dovydasvg@yahoo.fr>
2021-04-18 12:32:23 +0300

    added test for Sulfuras

--- php/tests/GildedRoseTest.php
+++ php/tests/GildedRoseTest.php
@@ -19,4 +19,15 @@ class GildedRoseTest extends TestCase
         $this->assertEquals(-1, $items[0]->sell_in);
         $this->assertEquals(0, $items[0]->quality);
     }
+
+    /* Sulfuras must never be sold and keep a quality of 80*/
+    public function testSulfuras(): void
+    {
+        $items = [new Item('Sulfuras, Hand of Ragnaros', 0, 80)];
+        $gildedRose = new GildedRose($items);
+        $gildedRose->updateQuality();
+        $this->assertSame('Sulfuras, Hand of Ragnaros', $items[0]->name);
+        $this->assertEquals(0, $items[0]->sell_in);
+        $this->assertEquals(80, $items[0]->quality);
+    }
 }
